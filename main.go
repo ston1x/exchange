f43df6f894d13da73901cd4992972b739dd4557d
Nicolai Stoianov <stoianovnk@gmail.com>
2024-12-28 22:33:58 +0100

    Respond with example quote

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Quote struct {
+	Symbol1 string
+	Symbol2 string
+	Price   float64
+}
+
+type QuotesHandler struct {
+	store quoteStore
+}
+
+type quoteStore interface {
+	Get(symbol1, symbol2 string) (Quote, error)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	router := gin.Default()
+
+	router.GET("/", homePage)
+
+	router.Run()
+}
+
+func homePage(c *gin.Context) {
+	quote := Quote{"btc", "usd", 100000}
+	c.JSON(http.StatusOK, quote)
 }
